2fdf625a45dcc736672a6a54530f4b04cdde1f6b
Gopal Joshi <uday_joshi69@yahoo.com>
2018-01-31 18:45:16 +0530

    practice updated
    practice updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  loadFeature = 'recipe';
+
+  onNavigation(featureEvent: string){
+    this.loadFeature = featureEvent;
+  }
 }
