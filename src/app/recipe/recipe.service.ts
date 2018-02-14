8bda9ab9784610343cf5d4b1371005ea7aa860da
Gopal Joshi <uday_joshi69@yahoo.com>
2018-02-14 16:16:39 +0530

    practice recipe updated

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs/Subject";
 
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
@@ -6,6 +7,7 @@ import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
 @Injectable()
 export class RecipeService {
+    recipeChanged = new Subject<Recipe[]>();
     private Recipes: Recipe[] = [
         new Recipe(
             'Greek Kale Salad with Quinoa & Chicken',
@@ -37,5 +39,19 @@ export class RecipeService {
 
     addIngredientToShoopingList(ingredients: Ingredient[]) {
         this.slService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe){
+        this.Recipes.push(recipe);
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.Recipes[index] = newRecipe;
+        this.recipeChanged.next(this.Recipes.slice());
+    }
+
+    deleteRecipe(index: number) {
+        this.Recipes.splice(index, 1);
+        this.recipeChanged.next(this.Recipes.slice());
     }
 }
